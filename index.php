45cef77086b0fa690d5a48b0d78ee2dc2bf6808a
Irina Babkina <irina.babkina36@gmail.com>
2022-10-14 09:18:48 +0300

    implements

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
 class Job {
+  private $logger;
+
+	public function __construct(ConsoleLogger $logger)
+	{
+		$this -> logger = $logger;
+	}
 	public function work(){
 		$logger = new ConsoleLogger();
 		for($i=0; $i<10; $i++){
@@ -9,13 +15,23 @@ class Job {
 	}
 }
 
-class ConsoleLogger{
+class ConsoleLogger implements Logger {
 	public function log($message){
 		echo $message . "\n";
 	}
 }
-
+interface Logger {
+	public function log($message);
+}
 
 //======================
-$job = new Job();
+
+class NothingLogger{
+	public function log($message){
+
+	}
+}
+$consoleLogger = new ConsoleLogger();
+$nothingLogger = new NothingLogger();
+$job = new Job($ConsoleLogger);
 $job ->work();
